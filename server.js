c10a108968cf0f242fcd3cdaa4de2a9ff4e23190
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-05-15 11:38:30 -0700

    basic response setup!

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-
 // dependencies
 require("dotenv").config();
 const PORT = process.env.PORT;
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 mongoose.connect(
   process.env.MONGO_URI,
   { useNewUrlParser: true, useUnifiedTopology: true },
@@ -14,21 +13,15 @@ mongoose.connect(
   }
 );
 
-
-
 // middleware
-app.use(cors());// prevents cors error
-app.use(express.json());// allows server to parse Json
+app.use(cors()); // prevents cors error
+app.use(express.json()); // allows server to parse Json
 app.use(express.urlencoded({ extended: true }));
-
-
 
 // ROUTES
 app.get("/", (req, res) => {
-  res.json(myBugs);
+  res.json("Back End Connected!");
 });
-
-
 
 app.listen(PORT, () => {
   console.log("Bugtracking at", PORT);

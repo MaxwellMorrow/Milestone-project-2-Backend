ca99027d554c739a17905aa5214151704dbf676f
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-05-16 13:40:59 -0700

    back end sending bugs to frontend!

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // dependencies
 require("dotenv").config();
-const PORT = process.env.PORT;
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
+
+// PORT AND MONGOOSE CONNECTION
+const PORT = process.env.PORT;
 const mongoose = require("mongoose");
 mongoose.connect(
   process.env.MONGO_URI,
@@ -13,21 +16,25 @@ mongoose.connect(
   }
 );
 
-// models connections
-const User = require("./models/User")
-const Bug = require("./models/Bug")
 
 // middleware
 app.use(cors()); // prevents cors error
 app.use(express.json()); // allows server to parse Json
 app.use(express.urlencoded({ extended: true }));
 
+
+// models connections
+const User = require("./models/User")
+const Bug = require("./models/Bug")
+
 // ROUTES
-app.get("/", async (req, res) => {
+
+// show all index route
+app.get("/bugs", async (req, res) => {
    try {
      const Bugs = await Bug.find();
      console.log(Bugs);
-     res.json(Bugs);
+     res.send(Bugs);
    } catch (e) {
      console.log(e.message);
      res.send(e)
